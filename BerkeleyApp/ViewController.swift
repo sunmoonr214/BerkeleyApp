4ac745aa9a30835536a1bc751d5b304f2cebe242
Ray Wan <raywan@berkeley.edu>
2022-04-09 14:46:09 -0700

    Initial Commit

---
+++ BerkeleyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BerkeleyApp
+//
+//  Created by Ray Wan on 2022/4/9.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
